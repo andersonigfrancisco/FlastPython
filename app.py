16f13d5b75aacc08575829f4b3fcae409a00da42
root <root@MambooLab>
2024-09-11 11:19:07 +0100

    feat: create route get

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from model.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hell_world():
-  return "hello world"
+tasks = []
+task_id=1
 
-@app.route("/about")
-def about():
-  return "Sobre a pagin"
+@app.route('/tasks',methods=['POST'])
+def create_task():
+  global task_id
+  data = request.get_json()
+  new_task = Task(id=task_id,title=data['title'],description=data['description'])
+  task_id +=1
+  tasks.append(new_task)
+  print(tasks)
+  return "teste"
 
+@app.route('/tasks', methods=['GET'])
+def get_task():
+  
+  task_list = [Task.to_dict() for Task in tasks]
+
+  output = {
+    "tasks":task_list,
+    "total_task":""
+  }
+  return jsonify(output)
+  
 if __name__ == "__main__":
   app.run(debug=True)
